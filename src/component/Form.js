a415e7a740be976e517eb9bd7fa6ce48093771f3
dominic <dominicseduraja3@gmail.com>
2023-06-05 22:52:01 +0530

    design start

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import FormSignup from './FormSignup'
+import "./Form.css"
 
 const Form = () => {
   return (
-    <div>
-        <FormSignup />
-    </div>
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>x</span>
+          <div className='form-content-left'>
+             <img src='https://image.pngaaa.com/897/2507897-middle.png' alt='left' className='form-img' />
+          </div>
+          <FormSignup />
+      </div>
+    </>
   )
 }
 
